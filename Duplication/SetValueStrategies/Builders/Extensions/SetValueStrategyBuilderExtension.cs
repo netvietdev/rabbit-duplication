17be3b86ac330510e15fba0271bac7adb4bf3aef
juanonsoftware <huanhvhd@gmail.com>
2015-04-02 18:49:01 +0700

    overload strategy

---
+++ Duplication/SetValueStrategies/Builders/Extensions/SetValueStrategyBuilderExtension.cs
@@ -0,0 +1,28 @@
+﻿using Duplication.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Duplication.SetValueStrategies.Builders.Extensions
+{
+    public static class SetValueStrategyBuilderExtension
+    {
+        public static IDictionary<Expression<Func<T, object>>, ISetValueStrategy> MergeWith<T>(
+            this ISetValueStrategyBuilder<T> primaryBuilder,
+            ISetValueStrategyBuilder<T> secondaryBuilder)
+        {
+            var results = primaryBuilder.Build();
+
+            foreach (var strategyKvp in secondaryBuilder.Build())
+            {
+                if (results.All(kvp => kvp.Key.GetPropertyInfo().Name != strategyKvp.Key.GetPropertyInfo().Name))
+                {
+                    results.Add(strategyKvp);
+                }
+            }
+
+            return results;
+        }
+    }
+}
