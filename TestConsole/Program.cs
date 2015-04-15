60e813787a12d1fb333eb8c2eb9e7be75cb38c46
juanonsoftware <huanhvhd@gmail.com>
2015-04-15 23:12:37 +0700

    Version 1.0.0-RC1

---
+++ TestConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Test.Model;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Arrange
+            var w1 = new Worker();
+            w1.SetAge(10);
+
+            // Act
+            var w2 = w1.Clone();
+
+            Console.WriteLine("Done");
+        }
+    }
+}
